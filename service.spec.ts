0e57f0ef9ac758eb6121adff9b8123fcc2cd26fb
Smartysonu <37367512+Smartysonu@users.noreply.github.com>
2025-03-24 18:09:29 +0530

    Update service.spec.ts

--- service.spec.ts
+++ service.spec.ts
@@ -1,46 +1,29 @@
-import { UiService } from './ui.service';
-import { TestBed } from '@angular/core/testing';
-import { UtilsService } from './utils.service';
-import * as _ from 'lodash';
-
-describe('UiService', () => {
-  let service: UiService;
-  let utilsService: UtilsService;
-  let slider: HTMLElement;
-  let currentElement: HTMLElement;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [UiService, UtilsService],
-    });
-
-    service = TestBed.inject(UiService);
-    utilsService = TestBed.inject(UtilsService);
-
-    // Mocking window.innerWidth for testing
-    spyOnProperty(window, 'innerWidth', 'get').and.returnValue(1000);
-
-    // Create mock slider and current element
-    slider = document.createElement('ul');
-    slider.classList.add('rs-metroline-mobile');
-    document.body.appendChild(slider);
-
-    currentElement = document.createElement('li');
-    currentElement.classList.add('rs-selected');
-    currentElement.style.left = '600px'; // This makes offsetLeft > window.innerWidth / 2
-    slider.appendChild(currentElement);
-
-    spyOn(slider, 'scrollLeft', 'set').and.callThrough(); // Spy on scrollLeft to check later
+beforeEach(() => {
+  // mock jQuery
+  (window as any).$ = jasmine.createSpy('$').and.returnValue({
+    foundation: jasmine.createSpy('foundation'),
+    on: jasmine.createSpy('on'),
+    css: jasmine.createSpy('css'),
   });
+});
 
-  it('should execute scrollLeft when offsetLeft > window.innerWidth / 2', () => {
-    // Spy on lodash.get to simulate 'offset Left' property for the current element
-    spyOn(_, 'get').and.returnValue(currentElement.offsetLeft);
+it('should not call chat method if hasChat is false', () => {
+  const mockInstance = { name: '', model: {} };
+  const mockCmpRef = { instance: mockInstance, hostView: {} as any };
 
-    // Call handleMetrolineCounterMobile method
-    service.handleMetrolineCounterMobile();
+  const vc = TestBed.inject(ViewContainerRef);
+  (vc.createComponent as jasmine.Spy).and.returnValue(mockCmpRef);
 
-    // Check if scrollLeft is set correctly
-    expect(slider.scrollLeft).toBe(currentElement.offsetLeft - window.innerWidth / 2 + 5);
-  });
+  const step = { path: 'step1' } as any;
+  const html = '<div></div>';
+  (component as any).vc = vc;
+  (component as any).injector = TestBed.inject(Injector);
+  (component as any).hasChat = false; // <-- important
+
+  component.createDynamicComponent(step, html);
+
+  expect((component as any).addChatWithUsFunctionality).not.toHaveBeenCalled();
+  expect((component as any).addLogicAfterRenderOfStep).toHaveBeenCalledWith('step1', {});
+  expect(component.stepComponentService.replaceKeyValues).toHaveBeenCalledWith(html);
+  expect(vc.insert).toHaveBeenCalledWith(mockCmpRef.hostView);
 });
