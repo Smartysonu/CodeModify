6c1166c7ce8d753ff4c949f47cec7da07f9bfea5
Smartysonu <37367512+Smartysonu@users.noreply.github.com>
2025-05-02 18:22:45 +0530

    Update new.java

--- new.java
+++ new.java
@@ -1,116 +1,120 @@
-package ext.cummins.part.dataUtilities;
+@Override
+public ComponentConfig buildComponentConfig(ComponentParams params) throws WTException {
+    LOGGER.debug("Enter >> DiscussionHistoryTableBuilder");
 
-import com.ptc.core.components.descriptor.ModelContext;
-import com.ptc.core.components.factory.dataUtilities.DefaultDataUtility;
-import com.ptc.core.components.rendering.guicomponents.AttributeGuiComponent;
-import com.ptc.core.components.rendering.guicomponents.UrlDisplayComponent;
-import wt.fc.ObjectReference;
-import wt.fc.ReferenceFactory;
-import wt.httpgw.GatewayServletHelper;
-import wt.httpgw.URLFactory;
-import wt.part.WTPart;
-import wt.util.WTException;
-import wt.workflow.forum.DiscussionForum;
-import wt.workflow.forum.DiscussionTopic;
-import wt.workflow.forum.ForumHelper;
+    ComponentConfigFactory factory = getComponentConfigFactory();
+    TableConfig table = factory.newTableConfig();
+    table.setLabel("Discussion History");
+    table.setId("ext.cummins.part.mvc.builders.CumminsDiscussionHistoryTableBuilder");
+    table.setSelectable(true);
+    table.setShowCount(true);
+    table.setActionModel("unsubscribed forum without delete actions");
+    table.setShowCustomViewLink(false);
+    table.setHelpContext("");
+    table.setRowBasedObjectHandle(true);
+    table.setShowCustomViewLink(true);
 
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+    // Add existing columns
+    ColumnConfig col1 = factory.newColumnConfig(ICON, true);
+    col1.setLabel(TYPE);
+    table.addComponent(col1);
 
-public class CumminsGetDiscussionDataUtility extends DefaultDataUtility  {
+    ColumnConfig col2 = factory.newColumnConfig("versionInfo.identifier.versionId", true);
+    col2.setLabel(VERSION_VIEW_DISPLAY_NAME);
+    table.addComponent(col2);
 
-    
+    // Discussion Link (col3)
+    ColumnConfig col3 = factory.newColumnConfig("url", false);  // Use this for URL display
+    col3.setDataUtilityId("getDiscussions");
+    col3.setLabel("Discussion Link");
+    table.addComponent(col3);
+
+    // Subject (col4)
+    ColumnConfig col4 = factory.newColumnConfig("subject", false);
+    col4.setDataUtilityId("getDiscussions");
+    col4.setLabel("Subject");
+    table.addComponent(col4);
+
+    // Message (col5)
+    ColumnConfig col5 = factory.newColumnConfig("message", false); // Display message
+    col5.setDataUtilityId("getDiscussions");
+    col5.setLabel("Message");
+    table.addComponent(col5);
+
+    // Created Date (col6)
+    ColumnConfig col6 = factory.newColumnConfig("createdDate", false);
+    col6.setDataUtilityId("getDiscussions");
+    col6.setLabel("Created Date");
+    table.addComponent(col6);
+
+    // Modification Date (col7)
+    ColumnConfig col7 = factory.newColumnConfig("modificationDate", false);
+    col7.setDataUtilityId("getDiscussions");
+    col7.setLabel("Modification Date");
+    table.addComponent(col7);
+
+    LOGGER.debug("End >> CumminsDiscussionHistoryTableBuilder");
+    return table;
+}
 
 @Override
-public Object getDataValue(String s, Object o, ModelContext modelContext) throws WTException {
-    System.out.println("s:" + s);
-    List<AttributeGuiComponent> topicList = new ArrayList<>();
+public Object buildComponentData(ComponentConfig config, ComponentParams paramComponentParams) throws WTException, IOException {
+    NmHelperBean nmHelperBean = ((JcaComponentParams) paramComponentParams).getHelperBean();
+    NmCommandBean nmCommandBean = nmHelperBean.getNmCommandBean();
 
-    if (o instanceof WTPart) {
-        WTPart part = (WTPart) o;
-        Enumeration<?> forums = ForumHelper.service.getForums(part);
+    Persistable requestObj = nmCommandBean.getPrimaryOid().getWtRef().getObject();
+    WTPart wtpart = null;
+    ArrayList<Object> listobj = new ArrayList<>();
+    
+    if (requestObj instanceof WTPart) {
+        wtpart = (WTPart) requestObj;
+        System.out.println("Request object is a WTPart: " + wtpart.getDisplayIdentifier());
 
-        if (forums.hasMoreElements()) {
-            DiscussionForum forum = (DiscussionForum) forums.nextElement();
-            Enumeration<?> topics = forum.getTopics();
+        // Query for all versions
+        QueryResult versionQuery = VersionControlHelper.service.allVersionsOf(wtpart);
+        if (versionQuery.size() == 0) {
+            System.out.println("No versions found for part: " + wtpart.getDisplayIdentifier());
+        } else {
+            System.out.println("Found " + versionQuery.size() + " versions for part: " + wtpart.getDisplayIdentifier());
+        }
 
-            while (topics.hasMoreElements()) {
-                DiscussionTopic topic = (DiscussionTopic) topics.nextElement();
-                Enumeration<?> postings = topic.getPostings();
+        Set<String> versionIdentifiers = new HashSet<>();
+        while (versionQuery.hasMoreElements()) {
+            Versioned versioned = (Versioned) versionQuery.nextElement();
+            if (versioned instanceof WTPart) {
+                WTPart versionPart = (WTPart) versioned;
+                String versionIdentifier = versionPart.getVersionIdentifier().getValue();
+                System.out.println("versionIdentifier: " + versionIdentifier);
 
-                while (postings.hasMoreElements()) {
-                    DiscussionPosting posting = (DiscussionPosting) postings.nextElement();
+                // Fetch the data from CumminsGetDiscussionDataUtility
+                CumminsGetDiscussionDataUtility discussionUtility = new CumminsGetDiscussionDataUtility();
+                List<List<String>> topicList = (List<List<String>>) discussionUtility.getDataValue("col5", versionPart, null);
 
-                    // Extract required details
-                    String message = posting.getBody(); // Posting message
-                    Timestamp created = posting.getCreateTimestamp();
-                    Timestamp modified = posting.getModifyTimestamp();
-                    String modifiedBy = posting.getModifierFullName(); // or use getModifiedBy() if needed
+                // Process the data and add it to listobj
+                for (List<String> topicData : topicList) {
+                    String url = topicData.get(0);  // URL for Discussion Link
+                    String subject = topicData.get(1);  // Subject
+                    String message = topicData.get(2);  // Message
+                    String createdDate = topicData.get(3);  // Created Date
+                    String modificationDate = topicData.get(4);  // Modification Date
 
-                    // Format string to show in column (can be enhanced with HTML or line breaks)
-                    String displayText = "Topic: " + topic.getName() +
-                            "\nMessage: " + message +
-                            "\nCreated: " + created +
-                            "\nModified: " + modified +
-                            "\nModified By: " + modifiedBy;
+                    // Add data to row for this topic
+                    HashMap<String, Object> row = new HashMap<>();
+                    row.put("url", url);  // URL for Discussion Link
+                    row.put("subject", subject);  // Subject
+                    row.put("message", message);  // Message
+                    row.put("createdDate", createdDate);  // Created Date
+                    row.put("modificationDate", modificationDate);  // Modification Date
 
-                    UrlDisplayComponent urlDisplay = new UrlDisplayComponent(s, null, null, null);
-
-                    // Create link to topic
-                    HashMap<String, String> hashmap = new HashMap<>();
-                    hashmap.put("action", "ObjProps");
-                    ObjectReference objectreference = ObjectReference.newObjectReference(topic);
-                    ReferenceFactory referencefactory = new ReferenceFactory();
-                    hashmap.put("oid", referencefactory.getReferenceString(objectreference));
-                    URLFactory urlfactory = new URLFactory();
-                    String url = GatewayServletHelper.buildAuthenticatedHREF(
-                            urlfactory,
-                            "wt.enterprise.URLProcessor",
-                            "URLTemplateAction",
-                            null,
-                            hashmap
-                    );
-
-                    urlDisplay.setLink(url);
-                    urlDisplay.setLabel(topic.getName());
-                    urlDisplay.setName(topic.getName());
-                    urlDisplay.setLabelForTheLink(displayText);  // This will show the full content on hover
-                    urlDisplay.setTarget("_blank");
-
-                    topicList.add(urlDisplay);
+                    listobj.add(row);
+                    System.out.println("Fetched version: " + versionPart.getDisplayIdentifier());
                 }
             }
         }
+    } else {
+        LOGGER.debug("Request object is not a WTPart.");
     }
 
-    return topicList;
-}}
---> in this code we we are displayText  in urlDisplay, but i need to add field on column value like-->  ColumnConfig col3 = factory.newColumnConfig("url", false);
-        col3.setDataUtilityId("getDiscussions");
-        col3.setLabel("Discussion Link");
-        table.addComponent(col3);
-
-        ColumnConfig col4 = factory.newColumnConfig("subject", false);
-        col4.setDataUtilityId("getDiscussions");
-        col4.setLabel("Subject");
-        table.addComponent(col4);
-
-        ColumnConfig col5 = factory.newColumnConfig("message", false); // false = NOT multiple
-        col5.setLabel("Message");
-        col5.setDataUtilityId("getDiscussions");
-        table.addComponent(col5);
-
-        ColumnConfig col6 = factory.newColumnConfig("createdDate", false);
-        col6.setDataUtilityId("getDiscussions");
-        col6.setLabel("Created Date");
-        table.addComponent(col6);
-
-        ColumnConfig col7 = factory.newColumnConfig("modificationDate", false);
-        col7.setDataUtilityId("getDiscussions");
-        col7.setLabel("Modification Date");
-        table.addComponent(col7);
-		
+    LOGGER.debug("Total elements fetched: " + listobj.size());
+    return listobj;
 }
