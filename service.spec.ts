3550a909a32bccb409b4aeddeea24d7b016a2e06
sonia-sarvar <40298696+sonia-sarvar@users.noreply.github.com>
2025-05-05 13:43:09 +0530

    Update service.spec.ts

--- service.spec.ts
+++ service.spec.ts
@@ -2,6 +2,9 @@ package ext.cummins.part.dataUtilities;
 
 import com.ptc.core.components.descriptor.ModelContext;
 import com.ptc.core.components.factory.dataUtilities.DefaultDataUtility;
+import com.ptc.core.components.rendering.guicomponents.TextDisplayComponent;
+import com.ptc.core.components.rendering.guicomponents.UrlDisplayComponent;
+
 import wt.fc.ObjectReference;
 import wt.fc.ReferenceFactory;
 import wt.httpgw.GatewayServletHelper;
@@ -9,23 +12,25 @@ import wt.httpgw.URLFactory;
 import wt.part.WTPart;
 import wt.util.WTException;
 import wt.workflow.forum.DiscussionForum;
+import wt.workflow.forum.DiscussionPosting;
 import wt.workflow.forum.DiscussionTopic;
 import wt.workflow.forum.ForumHelper;
-import wt.workflow.forum.DiscussionPosting;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 
 public class CumminsGetDiscussionDataUtility extends DefaultDataUtility {
 
     @Override
-    public Object getDataValue(String s, Object o, ModelContext modelContext) throws WTException {
-        List<List<String>> topicList = new ArrayList<>();
+    public Object getDataValue(String columnName, Object object, ModelContext modelContext) throws WTException {
+   System.out.println("columnName:" + columnName);
+        List<Object> components = new ArrayList<>();
 
-        if (o instanceof WTPart) {
-            WTPart part = (WTPart) o;
+        if (object instanceof WTPart) {
+            WTPart part = (WTPart) object;
             Enumeration<?> forums = ForumHelper.service.getForums(part);
 
             if (forums.hasMoreElements()) {
@@ -39,47 +44,58 @@ public class CumminsGetDiscussionDataUtility extends DefaultDataUtility {
                     while (postings.hasMoreElements()) {
                         DiscussionPosting posting = (DiscussionPosting) postings.nextElement();
 
-                        // Extract required details
-                        String message = posting.getBody(); // Posting message
+                        String subject = topic.getName() != null ? topic.getName() : "";
+                        String message = posting.getMessage() != null ? posting.getMessage() : "";
                         Timestamp created = posting.getCreateTimestamp();
                         Timestamp modified = posting.getModifyTimestamp();
-                        String modifiedBy = posting.getModifierFullName(); // or use getModifiedBy() if needed
 
-                        // Prepare URL for topic
-                        String url = buildUrl(topic);
+                        switch (columnName) {
+                            case "subject":
+                                components.add(new TextDisplayComponent("subject", subject));
+                                break;
+                            case "message":
+                                components.add(new TextDisplayComponent("message", message));
+                                break;
+                            case "createdDate":
+                                components.add(new TextDisplayComponent("createdDate", created != null ? created.toString() : ""));
+                                break;
+                            case "modificationDate":
+                                components.add(new TextDisplayComponent("modificationDate", modified != null ? modified.toString() : ""));
+                                break;
+                           case "url":
+			    UrlDisplayComponent urlDisplay = new UrlDisplayComponent(columnName, null, null, null);
+			     // Create link to topic
+                    HashMap<String, String> hashmap = new HashMap<>();
+                    hashmap.put("action", "ObjProps");
+                    ObjectReference objectreference = ObjectReference.newObjectReference(topic);
+                    ReferenceFactory referencefactory = new ReferenceFactory();
+                    hashmap.put("oid", referencefactory.getReferenceString(objectreference));
+                    URLFactory urlfactory = new URLFactory();
+                    String url = GatewayServletHelper.buildAuthenticatedHREF(
+                            urlfactory,
+                            "wt.enterprise.URLProcessor",
+                            "URLTemplateAction",
+                            null,
+                            hashmap
+                    );
 
-                        // Prepare row data for each topic
-                        List<String> rowData = new ArrayList<>();
-                        rowData.add(url);  // URL for Discussion Link (col3)
-                        rowData.add(topic.getName());  // Subject (col4)
-                        rowData.add(message);  // Message (col5)
-                        rowData.add(created.toString());  // Created Date (col6)
-                        rowData.add(modified.toString());  // Modification Date (col7)
+			    //String topicName = topic.getName() != null ? topic.getName() : "Untitled Topic";
+			    urlDisplay.setLink(url);
+		            urlDisplay.setLabel(topic.getName());
+		            urlDisplay.setName(topic.getName());
+		            urlDisplay.setTarget("_blank");
 
-                        // Add the row data to the list
-                        topicList.add(rowData);
+                            components.add(urlDisplay);
+			  
+                            System.out.println("Generated URL for topic: " + topic.getName() + " -> " + url);
+			    break;              
+                         }
                     }
                 }
             }
         }
-        return topicList;
-    }
 
-    private String buildUrl(DiscussionTopic topic) {
-        // Prepare the URL for the topic
-        HashMap<String, Object> hashmap = new HashMap<>();
-        hashmap.put("action", "ObjProps");
-        ObjectReference objectReference = ObjectReference.newObjectReference(topic);
-        ReferenceFactory referenceFactory = new ReferenceFactory();
-        hashmap.put("oid", referenceFactory.getReferenceString(objectReference));
-        URLFactory urlFactory = new URLFactory();
-
-        return GatewayServletHelper.buildAuthenticatedHREF(
-                urlFactory,
-                "wt.enterprise.URLProcessor",
-                "URLTemplateAction",
-                null,
-                hashmap
-        );
+    
+        return components;
     }
 }
